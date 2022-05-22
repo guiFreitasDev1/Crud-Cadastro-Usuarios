3ea0535c234ccd168c5fb441661f3832d838c980
Guilherme Freitas <95315019+guiFreitasDev1@users.noreply.github.com>
2022-05-21 23:59:27 -0300

    1.2

--- estudos/src/App.js
+++ estudos/src/App.js
@@ -1,19 +1,32 @@
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
-
+import Inputcpf from './inputcpf';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Estudos</h1>
+      <div className='heading'>
+        <h1 className="title">Estudos</h1>
+      </div>
 
       <div className="dadosInput">
-      <input type="text"
-      placeholder="Digite seu nome completo..."
-      />
+        <label for="nomeC">Nome completo</label>
+        <input type="text"
+          placeholder="Digite seu nome completo..."
+          id='nomeC' />
+      </div>
+      <div className="dadosInput">
+        <label for="cpfC">CPF</label>
+        <Inputcpf />
+      </div>
+      <div className="dadosInput">
+        <label for="datanasciment0C">Data de nascimento</label>
+        <input type="date"
+          placeholder="Digite sua data de nascimento..."
+          id='datanasciment0C' />
+      </div>
       <button className="buttonInserir">
         <FiSearch size={25} color="#000" />
       </button>
-      </div>
     </div>
   );
 }
