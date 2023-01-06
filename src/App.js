c94407337b0284c3892ab65507eb4d2d9bc05c6d
Guilherme Freitas <95315019+guiFreitasDev1@users.noreply.github.com>
2023-01-06 11:49:58 -0300

    new Front  End

--- src/App.js
+++ src/App.js
@@ -1,139 +1,76 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { useRef } from 'react';
+import React, { useState } from 'react';
+//import { useRef } from 'react';
 import './styles.css';
-import { useForm } from "react-hook-form";
-import InputMask from 'react-input-mask';
-// import axios from 'axios';
+import Button from '@mui/material/Button';
+import { DataGrid } from '@mui/x-data-grid';
+import BasicModal from './Usuarios/Create';
 
 function App() {
-  // 21/10/2022 Estou tentando limpar os inputs, GUILHERME DO FUTURO VER A DOCUMENTAÇÃO E TENTAR RESOLVER ISSO  => https://react-hook-form.com/api/useform/reset
-  const dropDownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
-  const onClick = () => setIsActive(!isActive);
+  // Modal Mui 
+  const [openModal, setOpenModal] = useState(false)
+  console.log(openModal)
+  // Function modal Mui
+  const handleModal = () => {
+    setOpenModal(!openModal)
+  }
 
+  //Rows e Columns datagrid 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'primeiroNome', headerName: 'Primeiro nome', width: 130 },
+    { field: 'ultimoNome', headerName: 'Ultimo Nome', width: 130 },
+    {
+      field: 'Idade',
+      headerName: 'Idade',
+      type: 'number',
+      width: 90,
+    },
 
-  // limpar os inputs, até o momento nao está limpando o do CPF
-  const { register, handleSubmit, reset, resetForm } = useForm();
+    {
+      field: 'nomeCompleto',
+      headerName: 'Nome Completo',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.primeiroNome || ''} ${params.row.ultimoNome || ''}`,
+    },
+  ];
 
-  
-  const resetAsyncForm = useCallback(async () => {
-    const result = await fetch('./api/formValues.json');
-    reset(result);
-  }, [reset]);
-
-
-  useEffect(() => {
-    resetAsyncForm()
-  }, [resetForm, resetAsyncForm])
-
-  // 22/10/2022 O input cpf está sendo limpo, porém com 2 cliques, GUILHERME DO FUTURO TENTE RESOLVER ISSO 
-
-  // E CORRIGIR O ERRO "TypeError: onValid is not a function" AO CLICAR EM CANCELAR  => https://react-hook-form.com/api/useform/reset
-
-  // pegando valor do input 
-  const [value, setValue] = useState();
-  const handleChangeValue = (value) => {
-    setValue((prevValue) => ({
-      ...prevValue,
-      [value.target.name]: value.target.value,
-    }))
-  };
-  
-
-  // click button inserir
-
-  const handleClickButton = () => {
-    console.log(value)
-};
-
-  // 04/12/2022 requisicao get com axios
-
-  //  useEffect(()=>{
-  //     axios.get("https")
-  //  }, [])
-
+  const rows = [
+    { id: 1, ultimoNome: 'Snow', primeiroNome: 'Jon', Idade: 35 },
+    { id: 2, ultimoNome: 'Lannister', primeiroNome: 'Cersei', Idade: 42 },
+    { id: 3, ultimoNome: 'Lannister', primeiroNome: 'Jaime', Idade: 45 },
+    { id: 4, ultimoNome: 'Stark', primeiroNome: 'Arya', Idade: 16 },
+    { id: 5, ultimoNome: 'Targaryen', primeiroNome: 'Daenerys', Idade: null },
+    { id: 6, ultimoNome: 'Melisandre', primeiroNome: null, Idade: 150 },
+    { id: 7, ultimoNome: 'Clifford', primeiroNome: 'Ferrara', Idade: 44 },
+    { id: 8, ultimoNome: 'Frances', primeiroNome: 'Rossini', Idade: 36 },
+    { id: 9, ultimoNome: 'Roxie', primeiroNome: 'Harvey', Idade: 65 },
+    { id: 10, ultimoNome: 'Roxie', primeiroNome: 'Harvey', Idade: 65 },
+  ];
   return (
 
+
     <div className='container'>
-      <div className='heading'>
-        <h1 className="title">Estudos</h1>
+      <div style={{ height: 650, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
       </div>
-
-      <form onSubmit={handleSubmit()} className='formularioPrinc'>
-        <div>
-          <div className="dadosInput">
-            <label htmlFor='nomeC'>Nome completo</label>
-            <input
-              type="text"
-              placeholder="Digite seu nome completo..."
-              id='nomeC'
-              name='name'
-              {...register('nome')}
-              onChange={handleChangeValue} />
-          </div>
-          <div className="dadosInput">
-            <label htmlFor='cpfC'>CPF</label>
-            <InputMask type='text'
-              mask="999.999.999-99"
-              id='cpfC'
-              name='cpf'
-              {...register('cpf')}
-              onChange={handleChangeValue} />
-          </div>
+      
+      <Button onClick={handleModal}>Cadastrar Usuarios</Button>
+      <BasicModal open={openModal} setOpen={setOpenModal} />
 
 
-          <div className="dadosInput">
-            <label htmlFor='datanasciment0C'>Data de nascimento</label>
-            <input
-              type="date"
-              placeholder="Digite sua data de nascimento..."
-              id='datanasciment0C'
-              name='dataN'
-              {...register('dataN')}
-              onChange={handleChangeValue} />
-          </div>
-          <div className='menu-container'>
-            <button onClick={onClick} className="menu-button">
-              <span>Ativo/Inativo</span>
-            </button>
-            <nav ref={dropDownRef} className={`menu ${isActive ? "active" : "inactive"}`}>
-              <label htmlFor='Ativo' onChange={handleChangeValue}> Ativo </label>
-              <input type={'radio'} id="Ativo" name='inputAtivo' {...register("inputAtivo")}></input><br />
-              <label htmlFor='Inativo' onChange={handleChangeValue}> Inativo </label>
-              <input type={'radio'} id="Inativo" name='inputInativo' {...register("inputInativo")}></input>
-            </nav>
-
-            <button 
-            className='buttonInserir' 
-            onClick={() => handleClickButton()}
-        
-            >Inserir</button>
-
-
-            <button 
-            className='buttonCancelar' 
-            onClick={() => {
-              reset(formValues => ({
-                ...formValues, cpf: '', nome: '', dataN: '', inputInativo: '', inputAtivo: ''
-              }))
-            }}>Cancelar</button>
-          </div>
-
-        </div>
-
-      </form>
-      <div className='divPesquisa'>
-        <label htmlFor='pesquisa'>Pesquisar: </label>
-        <input type={'text'} id='pesquisa' placeholder='Pesquise sua identidade...'></input>
-        <br></br>
-
-        <button className='buttonPesquisaDeletarEnviar'>Deletar</button>
-        <button className='buttonPesquisaDeletarEnviar'>Editar</button>
-
-      </div>
     </div>
 
   );
+
 }
 
 export default App;
